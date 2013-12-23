9dae2bd79abf8c2ba97e33b20692ece326c0e319
scott1028 <mic1028002@gmail.com>
2013-12-23 20:06:32 +0800

    modified:   main.java

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
-import aa.ball;	// 記得要先告成 public 才可以存取( 單獨引用一個 file )
+import aa.ball;		// 記得要先告成 public 才可以存取( 單獨引用一個 file )
 
 // javac main.java -encoding utf-8 && java Demo
 
 class Demo{
 	public static void main(String[] args) {
 		System.out.println(1);
-		book.echo();
+		book.echo();	// 同一個層級(default package)的不需要引用, JVM會自己找。
 		ball.kick();
 	}
 }
